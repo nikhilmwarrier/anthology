c16b6fc7f598ab595fe70f3b5789d8b134c62edd
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2025-11-14 18:53:37 +0530

    Cleanup imports

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,5 +1,3 @@
-import TableOfContents from "../components/reader/TableOfContents.svelte";
-import SettingsPanel from "../components/settings/SettingsPanel.svelte";
 import Home from "../pages/Home.svelte";
 import NotFoundPage from "../pages/NotFoundPage.svelte";
 import ReaderPage from "../pages/ReaderPage.svelte";
