d4e5aceb25f2d9c573a499fee88a63b101ad7f64
Nikhil M Warrier <nikhilmwarrier@disroot.org>
2026-03-20 00:27:37 +0530

    Fullscreen support for web

--- src/js/helpers/systemBars.ts
+++ src/js/helpers/systemBars.ts
@@ -3,11 +3,15 @@ import { NavigationBar } from "@squareetlabs/capacitor-navigation-bar";
 import { PLATFORM } from "../constants";
 
 export async function showSystemBars() {
-  if (PLATFORM === "web") return;
+  if (PLATFORM === "web") {
+    document.exitFullscreen();
+  }
   return Promise.all([StatusBar.show(), NavigationBar.show()]);
 }
 
 export async function hideSystemBars() {
-  if (PLATFORM === "web") return;
+  if (PLATFORM === "web") {
+    document.body.requestFullscreen();
+  }
   return Promise.all([StatusBar.hide(), NavigationBar.hide()]);
 }
